cba468e6535442a2c751dd6cb0919b5a0e87f875
yiguozhi <yiguozhi@alibaba-inc.com>
2018-11-20 18:13:16 +0800

    add fetool

---
+++ day01/src/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <div class="box1">
+			<text>12</text>
+		</div>
+    <div class="box2">
+			<text>34</text>
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+	.box1{
+		 width:200px;height: 200px;background: #41B883;
+	}
+</style>
